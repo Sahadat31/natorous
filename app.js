b93e416582a5d956ed759efea898ab155f3a1b8a
akash3108 <prince31081999@gmail.com>
2025-02-17 23:22:30 +0530

    refactor routes

--- app.js
+++ app.js
@@ -4,17 +4,27 @@ const app = express();
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 const port = 3000;
-app.use(express.json())
-app.get('/api/v1/tours', (req,res) => {
+const getAllTours = (req,res) => {
     res.status(200).json({
         status: "success",
         data: {
             tours
         }
     })
-})
-
-app.post('/api/v1/tours', (req,res) => {
+}
+const getTour = (req,res) => {
+    const {id} = req.params;
+    console.log(id)
+    const tour = tours.find(t=> t.id === +id)
+    res.status(200).json({
+        status: 'Success',
+        data: {
+            tour
+        }
+    })
+    
+}
+const createTour = (req,res) => {
     const newId = tours[tours?.length-1]?.id + 1
     const newTour = Object.assign({id: newId}, req.body)
     tours.push(newTour)
@@ -26,9 +36,8 @@ app.post('/api/v1/tours', (req,res) => {
             }
         })
     })
-})
-
-app.patch('/api/v1/tours/:id', (req,res) => {
+}
+const updateTour = (req,res) => {
     const {id} = req.params; 
     res.status(200).json({
         status: "Success",
@@ -36,9 +45,8 @@ app.patch('/api/v1/tours/:id', (req,res) => {
             tour: 'Updated'
         }
     })
-})
-
-app.delete('/api/v1/tours/:id', (req,res) => {
+}
+const deleteTour = (req,res) => {
     const {id} = req.params; 
     res.status(200).json({
         status: "Success",
@@ -46,20 +54,17 @@ app.delete('/api/v1/tours/:id', (req,res) => {
             tour: 'Deleted'
         }
     })
-})
+}
+app.use(express.json())
+// app.get('/api/v1/tours', getAllTours)
+// app.get('/api/v1/tours/:id', getTour)
+// app.post('/api/v1/tours', createTour)
+// app.patch('/api/v1/tours/:id', updateTour)
+// app.delete('/api/v1/tours/:id', deleteTour)
 
-app.get('/api/v1/tours/:id', (req,res) => {
-    const {id} = req.params;
-    console.log(id)
-    const tour = tours.find(t=> t.id === +id)
-    res.status(200).json({
-        status: 'Success',
-        data: {
-            tour
-        }
-    })
-    
-})
+app.route('/api/v1/tours').get(getAllTours).post(createTour)
+app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour)
+
 app.listen(port, () => {
     console.log(`App running on port ${port}`)
 })
